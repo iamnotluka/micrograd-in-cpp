aad0fa6c25901f53081fb449e3d7a180a116da40
iamnotluka <zoricluk@gmail.com>
2026-05-06 19:20:52 +1000

    Implement multiply operator

--- engine/include/value.h
+++ engine/include/value.h
@@ -12,6 +12,10 @@ class Value {
             return Value(data_ + other.data_);
         }
 
+        Value operator*(const Value& other) const {
+            return Value(data_ * other.data_);
+        }
+
     private:
         double data_;
 };
--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -3,9 +3,12 @@
 
 Value::Value(double data) : data_(data) {}
 
+// a*b + c = 10*5 + 4 = 54
 int main() {
     Value a(10);
     Value b(5);
-    std::cout << (a + b).data() << std::endl;
+    Value c(4);
+
+    std::cout << (a*b + c).data() << std::endl;
     return 0;
 };
