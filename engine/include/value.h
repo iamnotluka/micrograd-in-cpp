3aca68b99ea2fc29f93c4e1fbdc45bdc60384f86
iamnotluka <zoricluk@gmail.com>
2026-05-07 20:53:28 +1000

    Implement backpropagation function

--- engine/include/value.h
+++ engine/include/value.h
@@ -3,14 +3,15 @@
 #include <memory>
 #include <vector>
 #include <string>
+#include <functional>
 
 class Value {
     public:
         Value(double data, std::string label)
-            : data_(data), label_(std::move(label)), grad_(0) {}
+            : data_(data), label_(std::move(label)), grad_(0), backward_([](){}) {}
 
         Value(double data, std::vector<std::shared_ptr<Value>> prev, std::string op)
-            : data_(data), prev_(std::move(prev)), op_(std::move(op)), grad_(0) {}
+            : data_(data), prev_(std::move(prev)), op_(std::move(op)), grad_(0), backward_([](){}) {}
 
         static std::shared_ptr<Value> create(double data, std::string label) {
             return std::make_shared<Value>(data, std::move(label));
@@ -20,38 +21,34 @@ class Value {
             return std::make_shared<Value>(data, std::move(prev), std::move(op));
         }
 
-        double data() const {
-            return data_;
-        }
+        double data() const { return data_; }
 
-        std::string op() const {
-            return op_;
-        }
+        std::string op() const { return op_; }
 
-        std::string label() const {
-            return label_;
-        }
+        std::string label() const { return label_; }
 
-        void set_label(std::string label) {
-            label_ = std::move(label);
-        }
+        void set_label(std::string label) { label_ = std::move(label); }
 
-        std::vector<std::shared_ptr<Value>> prev() const {
-            return prev_;
-        }
+        std::vector<std::shared_ptr<Value>> prev() const { return prev_; }
 
-        double grad() const {
-            return grad_;
-        }
+        double grad() const { return grad_; }
 
-        void set_grad(double grad) {
-            grad_ = grad;
-        }
+        void set_grad(double grad) { grad_ = grad; }
+
+        void set_backward(std::function<void()> backward) { backward_ = std::move(backward); }
+
+        void backward() const { backward_(); };
 
         inline std::shared_ptr<Value> tanh(const std::shared_ptr<Value>& a) {       
             double x = a->data();
             double t = (std::exp(2*x) - 1) / (std::exp(2*x) + 1);
-            return Value::create(t, {a}, "tanh");
+
+            auto new_value = Value::create(t, {a}, "tanh");
+            new_value->set_backward([a, t, new_value]() {
+                a->set_grad(a->grad() + new_value->grad() * (1-t*t));
+            });
+
+            return new_value;
         }
 
     private:
@@ -60,22 +57,43 @@ class Value {
         std::string op_;
         std::string label_;
         double grad_;
+        std::function<void()> backward_;
 };
 
 inline std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
-    return Value::create(a->data() + b->data(), {a, b}, "+");
+    auto new_value = Value::create(a->data() + b->data(), {a, b}, "+");
+    new_value->set_backward([a, b, new_value]() {
+        a->set_grad(a->grad() + new_value->grad());
+        b->set_grad(b->grad() + new_value->grad());
+    });
+    return new_value;
 }
 
 inline std::shared_ptr<Value> operator-(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
-    return Value::create(a->data() - b->data(), {a, b}, "-");
+    auto new_value = Value::create(a->data() - b->data(), {a, b}, "-");
+    new_value->set_backward([a, b, new_value]() {
+        a->set_grad(a->grad() + new_value->grad());
+        b->set_grad(b->grad() - new_value->grad());
+    });
+    return new_value;
 }
 
 inline std::shared_ptr<Value> operator/(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
-    return Value::create(a->data() / b->data(), {a, b}, "/");
+    auto new_value = Value::create(a->data() / b->data(), {a, b}, "/");
+    new_value->set_backward([a, b, new_value]() {
+        b->set_grad(b->grad() + new_value->grad() * (-a->data() / (b->data() * b->data())));
+        a->set_grad(a->grad() + new_value->grad() * (1 / b->data()));
+    });
+    return new_value;
 }
 
 inline std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
-    return Value::create(a->data() * b->data(), {a, b}, "*");
+    auto new_value = Value::create(a->data() * b->data(), {a, b}, "*");
+    new_value->set_backward([a, b, new_value]() {
+        a->set_grad(a->grad() + new_value->grad() * b->data());
+        b->set_grad(b->grad() + new_value->grad() * a->data());
+    });
+    return new_value;
 }
 
 inline std::ostream& operator<<(std::ostream& os, const std::shared_ptr<Value>& value) {
--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -10,15 +10,21 @@ int main() {
     auto w1 = Value::create(-3, "w1");
     auto w2 = Value::create(1, "w2");
 
-    auto b = Value::create(6.7, "b");
+    auto b = Value::create(6.8813735870195432, "b");
 
     auto x1w1 = x1*w1; x1w1->set_label("x1*w1");
     auto x2w2 = x2*w2; x2w2->set_label("x2*w2");
     auto x1w1x2w2 = x1w1 + x2w2; x1w1x2w2->set_label("x1*w1 + x2*w2");
 
-    auto n = x1w1x2w2 + b;
+    auto n = x1w1x2w2 + b; n->set_label("n");
 
-    auto o = n->tanh(n);
+    auto o = n->tanh(n); o->set_label("o");
+    o->set_grad(1);
+    o->backward();
+    n->backward();
+    x1w1x2w2->backward();
+    x1w1->backward();
+    x2w2->backward();
 
     std::ofstream file("graph.dot");
     file << create_dot_graph_string(o);
